3a2f4e322787ba17f7b72895eca812363d5c9bd6
Takashi Aoki <takashi@Takashis-MBP.AirPort>
2022-07-08 12:42:42 +0900

    added nec installation

--- js/index.js
+++ js/index.js
@@ -4,9 +4,21 @@ const HeaderElement = `
         <p>Hello, I am a designer/visual artist specializing in CGI and human-computer interaction. Work in between 3D, motion graphics and software development.</p>
         <a href='./index.html'>Home</a> / 
         <a href='./bio.html'>Bio</a> / 
-        <a href='https://www.linkedin.com/in/chutaicho/'>Linkedin</a> / 
-        <a href='https://www.instagram.com/chutaicho/'>Instagram</a> / 
-        <a href='https://archive.takashiaoki.com/'>Archive</a>
+        <a href='https://www.linkedin.com/in/chutaicho/'  target='_blank'>Linkedin</a> / 
+        <a href='https://www.instagram.com/chutaicho/' target='_blank'>Instagram</a> / 
+        <a href='https://archive.takashiaoki.com/' target='_blank'>Archive</a>
+        <br><br>
+    </div>
+`;
+const HeaderSubElement = `
+    <div class='twelve columns' style='margin-top: 10%'>
+        <h2>Takashi Aoki</h2>
+        <p>Hello, I am a designer/visual artist specializing in CGI and human-computer interaction. Work in between 3D, motion graphics and software development.</p>
+        <a href='../index.html'>Home</a> / 
+        <a href='../bio.html'>Bio</a> / 
+        <a href='https://www.linkedin.com/in/chutaicho/'  target='_blank'>Linkedin</a> / 
+        <a href='https://www.instagram.com/chutaicho/' target='_blank'>Instagram</a> / 
+        <a href='https://archive.takashiaoki.com/' target='_blank'>Archive</a>
         <br><br>
     </div>
 `;
@@ -23,8 +35,23 @@ const FooterElement = `
         <a href=".">MAIL</a><br>
     </div>
 `;
+const FooterSubElement = `
+    <div class="six columns" style="margin-bottom: 10%">
+        <a href="../index.html">Home</a><br>
+        <a href="../bio.html">Bio</a><br>
+        <a href="https://archive.takashiaoki.com/" target="_blank">Past Archive</a><br>
+    </div>
+    <div class="six columns" style="margin-bottom: 10%">
+        <a href="https://www.linkedin.com/in/chutaicho/" target="_blank">Linkedin</a><br>
+        <a href="https://www.instagram.com/chutaicho/" target="_blank">Instagram</a><br>
+        <a href=".">MAIL</a><br>
+    </div>
+`;
 
+const location = document.location.pathname;
+//console.log(location.includes('/works'));
 const header = document.getElementById('header');
-header.innerHTML = HeaderElement;
+header.innerHTML = (location.includes('/works')) ? HeaderSubElement : HeaderElement;
 const footer = document.getElementById('footer');
-footer.innerHTML = FooterElement;
+footer.innerHTML = (location.includes('/works')) ? FooterSubElement : FooterElement;
+
